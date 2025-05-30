8a04999bb138088ff7b9343284d5d357569650ba
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-30 22:54:11 +0530

    feat: Add missing @Service annotation to EmployeeService class

--- sampleapp/src/main/java/com/system/sampleapp/service/EmployeeService.java
+++ sampleapp/src/main/java/com/system/sampleapp/service/EmployeeService.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
 
 import com.system.sampleapp.model.Employee;
 import com.system.sampleapp.repo.EmployeeRepo;
 
 import jakarta.persistence.EntityNotFoundException;
 
+@Service
 public class EmployeeService {
 	@Autowired
 	private EmployeeRepo repo;
